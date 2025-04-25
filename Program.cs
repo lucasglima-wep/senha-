bce3bcd5c0960bb5443b3b15a898b9efab3e4b82
lucas gabriel <lucasglimaosusa12@gmail.com>
2025-04-25 20:39:48 -0300

    lição de senha

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿string senhasdigitada;
+string senhasperada = "08230617";
+
+Console.Clear();
+
+Console.WriteLine("Digite A Senha ");
+senhasdigitada = Console.ReadLine()!;
+
+if (senhasdigitada == senhasperada)
+{ 
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("senha correta"); 
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("senha incorreta");
+}
+Console.ResetColor();
